8e3069ad42252c818498bd344d06814ba556e8b0
Tomomi ❤ Imura <tomomi.i@gmail.com>
2016-10-14 19:20:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# HTTP Status Cats for Slack
+
+Wanna quickly look up a definition of http status code on Slack? Just type `/httpstatus` slash command! HTTP Status Cats bot responds you with a short description of each status code with the [HTTP Status Cats](https://http.cat/) picture. 🐱
+
+## Running on Your Own
+
+When you clone this repo run on your own server, rename the `.env-test` file to `.env` and fill out your own Slack Dev credentials.
+
+## Install HTTP Status Cats Bot on Slack
+
+**TBD**
+I will deploy it on Heroku soon.
+Also I need to create a nicer web page.
+
+Authenticate from this button!
+
+[<img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /](https://slack.com/oauth/authorize?scope=commands+team%3Aread+&client_id=54308870179.89146186500)
+
+
+## About HTTP Status Cats
+
+Internet is made of cats, so why not HTTP Status codes, explained with cats?
+
+Originally, HTTP Status Cats was created by yours truly, **me** in 2011!
+It is best described on [Know Your Meme](http://knowyourmeme.com/memes/http-status-cats).
+
+I would never imagined I had such a 10-min fame at that time I created and posted the cat pics on [Flickr](https://www.flickr.com/photos/girliemac/sets/72157628409467125/). I was immediately featured on Boing Boing, Mashables, CNN Tech, BuzzFeed, and many more including international tech news etc. Especially, I loved that [Boing Boing](http://boingboing.net/2011/12/14/http-status-cats-by-girliemac.html) said I "just won the internet".
+
+A good time.
+
+HTTP Status Cats are later well-adopted by communities, and evolved into many APIs and apps, especially HTTP Status Cats API and its awesome domain, **http.cat** by [Versió Català](https://twitter.com/rogeriopvl). The page is also available in Catalan language, as its TLD shows!
---
+++ index.js
@@ -0,0 +1,111 @@
+'use strict';
+
+require('dotenv').config();
+const httpstatus = require('./httpstatus');
+const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = app.listen(3333, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+// Auth
+
+app.get('/slack', function(req, res){
+  var data = {form: {
+      client_id: process.env.SLACK_CLIENT_ID,
+      client_secret: process.env.SLACK_CLIENT_SECRET,
+      code: req.query.code
+  }};
+  request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // Get an auth token
+      let token = JSON.parse(body).access_token;
+
+      // Get the team domain name to redirect to the team URL after auth
+      request.post('https://slack.com/api/team.info', {form: {token: token}}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          if(JSON.parse(body).error == 'missing_scope') {
+            res.send('HTTP Status Cats has been added to your team!');
+          } else {
+            let team = JSON.parse(body).team.domain;
+            res.redirect('http://' +team+ '.slack.com');
+          }
+        }
+      });
+    }
+  })
+});
+
+/* *******************************
+/* HTTP Status Cats Slash Command
+/* ***************************** */
+
+app.get('/', (req, res) => {
+  handleQueries(req.query, res);
+});
+
+app.post('/', (req, res) => {
+  handleQueries(req.body, res);
+});
+
+/*
+response:
+{ token: '2P429UX-------',
+  team_id: 'T1L---',
+  team_domain: 'girliemac',
+  channel_id: 'C1L---',
+  channel_name: 'general',
+  user_id: 'U1L----',
+  user_name: 'girlie_mac',
+  command: '/httpstatus',
+  text: '405',
+  response_url: 'https://hooks.slack.com/commands/--- }
+*/
+
+function handleQueries(q, res) {
+  if(q.token !== process.env.SLACK_VERIFICATION_TOKEN) {
+    // the request is NOT coming from Slack!
+    return;
+  }
+  if (q.text) {
+    let code = q.text;
+
+    if(! /^\d+$/.test(code)) { // not a digit
+      res.send('U R DOIN IT WRONG. Enter a status code like 200 😒');
+      return;
+    }
+
+    let status = httpstatus[code];
+    if(!status) {
+      res.send('Bummer, ' + code + ' is not an official HTTP status code 🙃');
+      return;
+    }
+
+    let image = 'https://http.cat/' + code;
+    let data = {
+      response_type: 'in_channel', // public to the channle
+      text: code + ': ' + status,
+      attachments:[
+      {
+        image_url: image
+      }
+    ]};
+    res.json(data);
+  } else {
+    let data = {
+      response_type: 'ephemeral', // private message
+      text: 'How to use /httpstatus command:',
+      attachments:[
+      {
+        text: 'Type a status code after the command, e.g. `/httpstatus 404`',
+      }
+    ]};
+    res.json(data);
+  }
+}
