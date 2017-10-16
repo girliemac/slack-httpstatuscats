fc952af84191e13ffdea7775312e5628bcba9e54
raphaklaus <raphaklaus@gmail.com>
2017-10-16 00:11:40 -0200

    feat: add promise chain and better error handling

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const httpstatus = require('./httpstatus');
-const request = require('request');
+const request = require('request-promise-native');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const server = app.listen(process.env.PORT || 3333, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
@@ -15,34 +15,34 @@ const server = app.listen(process.env.PORT || 3333, () => {
 
 // Auth
 
-app.get('/slack', function(req, res){
+app.get('/slack', (req, res) => {
   if (!req.query.code) { // access denied
     res.redirect('http://www.girliemac.com/slack-httpstatuscats/');
     return;
   }
-  var data = {form: {
-      client_id: process.env.SLACK_CLIENT_ID,
-      client_secret: process.env.SLACK_CLIENT_SECRET,
-      code: req.query.code
+  let data = {form: {
+    'client_id': process.env.SLACK_CLIENT_ID,
+    'client_secret': process.env.SLACK_CLIENT_SECRET,
+    'code': req.query.code
   }};
-  request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      // Get an auth token
-      let token = JSON.parse(body).access_token;
+  let token;
+  request.post('https://slack.com/api/oauth.access', data, (response, body) => {
+    // Get an auth token
+    token = JSON.parse(body).access_token;
 
-      // Get the team domain name to redirect to the team URL after auth
-      request.post('https://slack.com/api/team.info', {form: {token: token}}, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          if(JSON.parse(body).error == 'missing_scope') {
-            res.send('HTTP Status Cats has been added to your team!');
-          } else {
-            let team = JSON.parse(body).team.domain;
-            res.redirect('http://' +team+ '.slack.com');
-          }
-        }
-      });
+    // Get the team domain name to redirect to the team URL after auth
+    return request.post('https://slack.com/api/team.info', {form: {token: token}});
+  }).then((response, body) => {
+    if (JSON.parse(body).error === 'missing_scope') {
+      res.send('HTTP Status Cats has been added to your team!');
+    } else {
+      let team = JSON.parse(body).team.domain;
+      res.redirect(`http://${team}.slack.com`);
     }
-  })
+  }).catch(error => {
+    res.send('Something went wrong, check the logs');
+    console.log(error);
+  });
 });
 
 /* *******************************
@@ -72,43 +72,44 @@ response:
 */
 
 function handleQueries(q, res) {
-  if(q.token !== process.env.SLACK_VERIFICATION_TOKEN) {
+  if (q.token !== process.env.SLACK_VERIFICATION_TOKEN) {
     // the request is NOT coming from Slack!
+    res.send('No-No. It seems you don\'t have access to do that.');
     return;
   }
   if (q.text) {
     let code = q.text;
 
-    if(! /^\d+$/.test(code)) { // not a digit
+    if (!/^\d+$/.test(code)) { // not a digit
       res.send('U R DOIN IT WRONG. Enter a status code like 200 😒');
       return;
     }
 
     let status = httpstatus[code];
-    if(!status) {
+    if (!status) {
       res.send('Bummer, ' + code + ' is not an official HTTP status code 🙃');
       return;
     }
 
     let image = 'https://http.cat/' + code;
     let data = {
-      response_type: 'in_channel', // public to the channle
-      text: code + ': ' + status,
-      attachments:[
-      {
-        image_url: image
-      }
-    ]};
+      'response_type': 'in_channel', // public to the channle
+      'text': code + ': ' + status,
+      'attachments': [
+        {
+          image_url: image
+        }
+      ]};
     res.json(data);
   } else {
     let data = {
-      response_type: 'ephemeral', // private message
-      text: 'How to use /httpstatus command:',
-      attachments:[
-      {
-        text: 'Type a status code after the command, e.g. `/httpstatus 404`',
-      }
-    ]};
+      'response_type': 'ephemeral', // private message
+      'text': 'How to use /httpstatus command:',
+      'attachments': [
+        {
+          text: 'Type a status code after the command, e.g. `/httpstatus 404`',
+        }
+      ]};
     res.json(data);
   }
 }
